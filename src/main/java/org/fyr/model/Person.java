a275629f0c9db4cb8ca05a635e43dc2cd1f5c98f
sebbe fyr <sebbe.fyhr@hotmail.com>
2025-07-03 13:38:51 +0200

    updated person class according to requirements

--- src/main/java/org/fyr/model/Person.java
+++ src/main/java/org/fyr/model/Person.java
@@ -1,30 +1,31 @@
 package org.fyr.model;
 
+import java.util.Objects;
+
 public class Person {
     private int id;
     private String firstName;
     private String lastName;
     private String email;
+    private AppUser credentials;
 
     public Person(){
 
     }
 
-    public Person(String firstName, String lastName, String email) {
+    public Person(int id, String firstName, String lastName, String email, AppUser creds) {
         if((firstName== null || firstName.trim().length() == 0) ||
                 (lastName == null || lastName.trim().length() == 0) ||
                 (email == null || email.trim().length() == 0)){
             throw new NullPointerException("Cant be null");
         }
+        this.id = id;
         this.firstName = firstName.trim();
         this.lastName = lastName.trim();
         this.email = email.trim();
+        this.credentials = creds;
     }
 
-    public Person(int id, String firstName, String lastName, String email) {
-        this(firstName, lastName, email);
-        this.id = id;
-    }
 
     public int getId() {
         return id;
@@ -68,13 +69,25 @@ public class Person {
         this.email = email;
     }
 
-    public String getSummary(){
-        return "{" +
-                "id: " + id +
-                ", name: " + firstName +" " + lastName  +
-                ", email: " + email  +
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
                 '}';
-
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(email, person.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email);
+    }
 }
