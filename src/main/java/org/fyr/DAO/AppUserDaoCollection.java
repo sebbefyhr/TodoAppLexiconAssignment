255d74b04b6af5f747eb3d4b878913d3fb69bd98
sebbe fyr <sebbe.fyhr@hotmail.com>
2025-07-21 14:32:46 +0200

    AppUserDaoCollection and PersonDaoCollection implemented, needs to be revisited.

--- src/main/java/org/fyr/DAO/AppUserDaoCollection.java
+++ src/main/java/org/fyr/DAO/AppUserDaoCollection.java
@@ -1,11 +1,16 @@
 package org.fyr.DAO;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.fyr.model.AppUser;
 
-import java.io.Serializable;
+import java.io.*;
+import java.lang.runtime.ObjectMethods;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
+import java.util.Optional;
 
 
 //TODO -- • Use the Stream API in implementing DAO classes
@@ -13,35 +18,87 @@ public class AppUserDaoCollection implements AppUserDAO<AppUser>, Serializable {
 
     private static AppUserDaoCollection appUserDaoCollection;
 
-    private AppUserDaoCollection(){}
+    private ObjectMapper mapper = new ObjectMapper();
 
+
+    private String file = "src/main/resources/protocols/Appuser.json";
+
+    // TODO -- Göra till Set istället för List???
+    private List<AppUser> appUsers;
+
+
+    private AppUserDaoCollection(){
+        appUsers = new ArrayList<>();
+    }
 
     @Override
     public AppUser persist(AppUser appUser) {
+        if(appUsers.contains(appUser)){
+            return appUser;
+        }
+        appUsers.add(appUser);
 
-        return null;
+        return appUser;
     }
 
     @Override
     public AppUser findByUsername(String username) {
 
-        return null;
+        //TODO -- Equals eller equalsIgnoreCase??
+
+         Optional<AppUser> appie  =  appUsers.stream()
+                .filter(s -> s.getUsername().equalsIgnoreCase(username))
+                .findFirst();
+
+        return appie.get();
     }
 
     @Override
     public Collection<AppUser> findAll() {
-        return List.of();
+
+        return appUsers;
     }
 
     @Override
     public void remove(String username) {
+        appUsers.removeIf(s ->
+                //TODO -- Equals eller equalsIgnoreCase??
+            s.getUsername().equalsIgnoreCase(username));
 
     }
 
     public static AppUserDaoCollection getInstance(){
         if(appUserDaoCollection == null){
             appUserDaoCollection = new AppUserDaoCollection();
+
         }
         return appUserDaoCollection;
+    }
+
+    public void loadAppusers(){
+
+        try(FileReader reader = new FileReader(file)){
+            appUsers = mapper.readValue(reader, new TypeReference<List<AppUser>>() {});
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveAppusers(){
+
+        try(FileWriter writer = new FileWriter(file)){
+
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            writer.write(mapper.writeValueAsString(appUsers).toString());
+
+            appUsers.clear();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/org/fyr/DAO/PersonDaoCollection.java
+++ src/main/java/org/fyr/DAO/PersonDaoCollection.java
@@ -1,47 +1,109 @@
 package org.fyr.DAO;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.fyr.model.Person;
 
-import java.util.Collection;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 //TODO -- • Use the Stream API in implementing DAO classes
-public class PersonDaoCollection implements PersonDAO{
+public class PersonDaoCollection implements PersonDAO, Serializable {
 
     private static PersonDaoCollection personDaoCollection;
 
-    private PersonDaoCollection(){}
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    private final File file = new File("src/main/resources/protocols/Person.json");
+
+    private List<Person> personList;
+
+    private PersonDaoCollection() {
+        personList = new ArrayList<>();
+    }
 
     @Override
     public Person persist(Person person) {
+        // TODO add check for email if exists, also
+        if(personList.contains(person)) {
+            return person;
+        }
+        personList.add(person);
 
-        return null;
+        return person;
     }
 
     @Override
     public Person findById(int id) {
-        return null;
+
+        Optional<Person> p = personList.stream()
+                .filter(s -> s.getId() == id)
+                .findFirst();
+
+        return p.get();
     }
 
     @Override
     public Person findByEmail(String email) {
-        return null;
+        Optional<Person> p = personList.stream()
+                .filter(s -> s.getEmail().equalsIgnoreCase(email))
+                .findFirst();
+
+        return p.get();
     }
 
     @Override
     public Collection<Person> findAll() {
-        return List.of();
+        return personList;
     }
 
     @Override
     public void remove(int id) {
+        personList.removeIf(s -> s.getId() == id);
 
     }
 
-    public static PersonDaoCollection getInstance(){
-        if(personDaoCollection == null){
+    public static PersonDaoCollection getInstance() {
+        if (personDaoCollection == null) {
             personDaoCollection = new PersonDaoCollection();
         }
         return personDaoCollection;
+    }
+
+    public void loadPersons(){
+
+        //TODO -- Rensa upp kommentarer.
+
+        mapper.configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, true);
+        try(FileReader reader = new FileReader(file)){
+
+            personList = mapper.readValue(reader, new TypeReference<List<Person>>(){});
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void savePersons(){
+        System.out.println("--------------------------------------\nSave Persons method\n--------------------------------------");
+        try(FileWriter writer = new FileWriter(file)) {
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            writer.write(mapper.writeValueAsString(personList).toString());
+
+            personList.clear();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
