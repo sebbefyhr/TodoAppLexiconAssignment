7ee3b000d533c858d9b3786adfa9eaaa84b0d419
sebbe fyr <sebbe.fyhr@hotmail.com>
2025-09-02 22:43:33 +0200

    DB class renamed, working as should

--- src/main/java/org/fyr/db/MySQLDBConnection.java
+++ src/main/java/org/fyr/db/MySQLDBConnection.java
@@ -4,13 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MySQLDataBase {
+public class MySQLDBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/todoit";
     private static final String USER = "root";
     private static final String PASS = "1234";
 
-    private MySQLDataBase(){
+    private MySQLDBConnection(){
 
     }
 
